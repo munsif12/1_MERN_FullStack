7f1b31a3afd9adfac35ccd7cd93d988493604448
Munsif <munsifalimisri69@gmail.com>
2021-04-02 17:45:47 +0500

    added middleware in schema to hash the passwrod befor calling the save() method

--- server/schema_or_models/userSchema.js
+++ server/schema_or_models/userSchema.js
@@ -5,6 +5,8 @@ Mongoose Schema vs. Model. A Mongoose model is a wrapper on the Mongoose schema.
 */
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 //creating the schema for userData schema matlab data kis shape ma para hua hoga like sql ma taables ka ander kon  fields hote h wasa
 const userSchema = new mongoose.Schema({
   userName: {
@@ -46,6 +48,19 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+//middleware hamesha modle banay sa phly call hogy wrna app kam to theak kry ga lakin iscase my hash nhi hoga just simple save hoga
+// making a hash for password
+//next ka parameter hona lazmii ma yahe to batai ga middleware ko ka next kya execute krna ha iscase ma next save() methods ko call kry ga
+userSchema.pre("save", async function (next) {
+  //userData.save() methods ko call krny sa phly => userschema.pre("kis_method_ko_exe_krny_sa_pahly -> save", async function(){})
+  //har save hony sa pahly pre middle ware challyga ab ham middleware ko ham speacifically bata rhy h ka agr passwprd modilfy ho to usko hash kro  bs
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cPassword = await bcrypt.hash(this.cPassword, 12);
+  }
+  next(); //will call the save() method
 });
 
 //creating model mtlab single user ka data {name:"sdsd",age:"etc"..etc}
