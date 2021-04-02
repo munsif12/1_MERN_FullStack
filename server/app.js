b36c698d9b2590793b016373677e95f095844a0d
Munsif <munsifalimisri69@gmail.com>
2021-04-02 17:46:45 +0500

    main App.js  exe file

--- server/app.js
+++ server/app.js
@@ -1,40 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 
-//connecting to online mongodb atlas -> u hava to have mongodb installedin your local machine
-const DB =
-  "mongodb+srv://munsif12:Munsif___69@cluster0.j3nyq.mongodb.net/MERN_FULLSTACK?retryWrites=true&w=majority";
-mongoose.connect(
-  DB,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  },
-  () => {
-    console.log("connection to mongodb atlas successfull");
-  }
-);
+//dotenv ka filePath app ko batanay kaleya
+dotenv.config({ path: "./config.env" });
+//settingn up connection by requiring the connetion file the connection functionality is performed in connection.js file
+require("./db_connection/connection");
 
-//adding a middle ware mtlab agr koi functionality perofrm krwany sa pahly kuch krwana hoto yani mtlab agr user direclty about page acces krrha h to check kro ka uskaalogin keya h k nhi pahly
-//responce ko pany sa pahly agr koi functionality krwanii hoto middle warre use hota ha
-//Middleware functions can perform the following tasks: Execute any code. Make changes to the request and the response objects.
-const checkLoginMiddleWare = (req, res, next) => {
-  console.log("Comming from MiddleWare :- user is logged in ");
-  next(); //next na lika to page pending pa rhy ga or ghoomta rhy ga
-  //jab middle ware ka kam khtm hojata ha to ham next() ka use excution ko continue krny ka keya krty ha agr next ko call nhi keya to program isemiddle ware ka ander rah jai ga or further execution nhi hota jab tak next na likha to
-};
+//app ko batow ka data json format ma aiy ga agr koi request postman ka zareya hui ha
+//app ko batarhy ha ka agr json ka data aiy to usko object ma convert krdo
+app.use(express.json()); //ya lines hamesha router ko require wali line sa uper hone chaheya becoz js synchroonous hota h wara mare tarah 20 min waste hongy
 
-//creating the routes
-app.get("/", checkLoginMiddleWare, (req, res) => {
-  res.send("hello from the Home page");
-});
-app.get("/about", (req, res) => {
-  res.send("hello from the About Page");
-});
+//getting the routes for the app like app.get("/",()=>{res.send("hello server")})
+app.use(require("./routes/routes"));
+
 //reserving port 8000 to listen for requests
-app.listen(8000, () => {
-  console.log("listening at port no  8000");
+app.listen(process.env.PORT, () => {
+  console.log(`Listening at port no ${process.env.PORT}`);
 });
