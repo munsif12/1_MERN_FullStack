f2415ac0a7b3885ef6651f9a384a7b84fbaf0916
Munsif <munsifalimisri69@gmail.com>
2021-04-05 20:05:54 +0500

    Contect Page of the project

---
+++ front_end/src/Components/Contect/Contect.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contect() {
+  return (
+    <div className="contect">
+      <h1>Hello From Contect Page</h1>
+    </div>
+  );
+}
+
+export default Contect;
