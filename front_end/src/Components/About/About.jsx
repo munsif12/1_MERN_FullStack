fa86c1d71ed7c0c06676db80950e57f3e0947b82
Munsif <munsifalimisri69@gmail.com>
2021-04-06 22:08:09 +0500

    added form to About page

--- front_end/src/Components/About/About.jsx
+++ front_end/src/Components/About/About.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap.css";
+import "./About.css";
 function About() {
   return (
     <div className="about">
-      <h1>Hello From About Page</h1>
+      <div className="aboutDetails">
+        <h1>User Details</h1>
+        <ul>
+          <li>
+            <label> Id </label> : <p> 232323232323</p>
+          </li>
+          <li>
+            <label>Name</label> : <p> Munsif</p>
+          </li>
+          <li>
+            <label>Email</label> : <p> xyz@gmail.com</p>
+          </li>
+          <li>
+            <label>Phone</label> : <p> 000001222</p>
+          </li>
+          <li>
+            <label>Profession</label> : <p> MERN STACK DEVELOPER</p>
+          </li>
+          <li>
+            <label>Name</label> : <p> Munsif</p>
+          </li>
+        </ul>
+      </div>
+      <div className="timelineDetails">
+        <h1>this is timeLine</h1>
+      </div>
     </div>
   );
 }
