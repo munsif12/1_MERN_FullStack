b54061ee38af462a5b9caf81e3a5ee00ea2832d1
Munsif <munsifalimisri69@gmail.com>
2021-04-06 22:09:07 +0500

    added Form to Contect page

--- front_end/src/Components/Contect/Contect.jsx
+++ front_end/src/Components/Contect/Contect.jsx
@@ -3,7 +3,37 @@ import React from "react";
 function Contect() {
   return (
     <div className="contect">
-      <h1>Hello From Contect Page</h1>
+      <div className="upper">
+        <div className="phoneDetail">
+          <p className="phone">Phone: 033531175557</p>
+        </div>
+        <div className="emailDetail">
+          <p className="email">Email :xyz@gmail.com</p>
+        </div>
+        <div className="addressDetail">
+          <p className="address">Address : block xyz XYZ</p>
+        </div>
+        <div className="getInTouch">
+          <p>Get In Touch</p>
+          <form action="">
+            <input type="text" name="name" id="name" placeholder=" Name. " />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder=" Email. "
+            />
+            <input type="text" name="phone" id="phone" placeholder=" Phone. " />
+            <input
+              type="text"
+              name="userMessage"
+              id="userMessage"
+              placeholder=" Message. "
+            />
+            <input type="submit" value="Submit" id="submit" name="submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
