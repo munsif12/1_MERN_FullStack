42a2a4bffa9d57233144b99c4871b9cbf4a71810
Munsif <munsifalimisri69@gmail.com>
2021-04-07 23:37:04 +0500

    Complete user registration is added with Complete Authentication

--- front_end/src/Components/Register/Register.jsx
+++ front_end/src/Components/Register/Register.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./Register.css";
 function Register() {
+  const history = useHistory();
   const [formFilds, setFormFilds] = useState({
     name: "",
     email: "",
@@ -10,14 +15,88 @@ function Register() {
     cPassword: "",
   });
   function getFormFieldsData(e) {
-    e.preventDefault();
     const fieldValue = e.target.value;
     const fieldName = e.target.name;
     setFormFilds({ ...formFilds, [fieldName]: fieldValue });
   }
+  async function submitData(e) {
+    e.preventDefault();
+    const { name, email, phone, work, password, cPassword } = formFilds;
+    if (!name || !email || !phone || !work || !password || !cPassword) {
+      toast.error(`🦄 All fields are mandatory to be Filled`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    /*fetch method ka zareya ham apna data /login page joka port 8000 pa run horha h waha send krrhy ha jasa ka ham pahly POSTMAN ma ka through krrhy thy*/
+    /*jasy postMan ma ham phly method then header then body choose krty thy isetrah fetch ka 2nd param ka object ma b ham wohe kam krrhy h
+    method :=> ham bata ha ka ham kya krna chahrhy ha like data save ya delete ya update etc (put,patch,post,delete)
+    headers :=> hamy document ka type batay ha like (content-type:Text/plain)(content-type:applicaton/json
+    body :=> isma hamara data hota ha joham frontend sa backend pa send krrhy hoty h 
+    json.stringify :=> [ body:json.stringify({ your js data here }) ] ya hamary javascript ko json ma convert karta ha q ka server sirf json data ko smajta h not javascript server sa bat krwany ka leya ya json ya xml use hote ha or hamjson use krrhy ha */
+    const response = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+        work,
+        password,
+        cPassword,
+      }),
+    });
+    //jo reponse hamay pas arha h usko js ma convert krrhy ha ham
+    const data = await response.json();
+    console.log(response.status);
+    if (
+      response.status === (502 || 422) ||
+      typeof data === "undefined" ||
+      !data
+    ) {
+      // alert(res.message);
+      toast.error(
+        `🦄 ${data.message || "somthing went wrong check you field"}`,
+        {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        }
+      );
+      console.log("inside error");
+    } else if (response.status === 200) {
+      // alert(res.message); /* alert ka sath to history.push kam krrha ha mtlabb alert ka wait krta ha then net page pa jata h
+      /*lakin toastify ma direct he push page pa challa jata without notify keya huay  SETIMEOUT WalA JUGAR NEaCHY LAGA H*/
+      toast.success(`${data.message}`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      // setTimeout wala jugar ha taka toastify ka notifaction ka wait kry or phr next history.push waly page pa jai
+      setTimeout(() => {
+        history.push("/login");
+      }, 2000);
+    }
+    // }
+  }
   return (
     <div className="register">
-      <form action="">
+      <form method="POST">
         <input
           type="text"
           name="name"
@@ -66,8 +145,15 @@ function Register() {
           value={formFilds.cPassword}
           onChange={getFormFieldsData}
         />
-        <input type="submit" value="Submit" name="submit" id="submit" />
+        <input
+          type="submit"
+          value="Submit"
+          name="submit"
+          id="submit"
+          onClick={submitData}
+        />
       </form>
+      <ToastContainer />
     </div>
   );
 }
