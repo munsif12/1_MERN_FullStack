d08d763fc6ac53de7548fb6dbee11e215a8d93a7
Munsif <munsifalimisri69@gmail.com>
2021-04-08 22:27:22 +0500

    Working on home page UI

--- front_end/src/Components/Home/Home.jsx
+++ front_end/src/Components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./home.css";
+import SvgWaveBackgroundBottom from "./SvgWaveBackgroundBottom";
+import SvgBlackLiveMatter from "./SvgBlackLiveMatter";
 
 function Home() {
   return (
     <div className="home">
-      <h1>Hello From Home Page</h1>
+      <div className="home__content">
+        <div className="home__content__logoLeft">
+          <div className="logo">
+            <SvgBlackLiveMatter />
+          </div>
+        </div>
+        <div className="home_content__welcomeTextRight">
+          <h1>BLACK LIVES MATTER</h1>
+        </div>
+      </div>
+
+      {/* bottom wave */}
+      <div className="backgroundSvgWave">
+        <SvgWaveBackgroundBottom />
+      </div>
     </div>
   );
 }
