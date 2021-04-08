1aaee95007a44d47feb9a9223bb8abb6507d771a
Munsif <munsifalimisri69@gmail.com>
2021-04-08 22:29:04 +0500

    Navigation bar almost done

--- front_end/src/Components/Navbar/Navbar.jsx
+++ front_end/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Navbar.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="##">
-          Navbar
+        <NavLink className="navbar-brand navbar_logo" to="##">
+          MUNSIF
         </NavLink>
         <button
           className="navbar-toggler"
@@ -22,28 +23,54 @@ function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <NavLink className="nav-link" to="/">
-                Home <span className="sr-only">(current)</span>
+            <li className="nav-item ">
+              <NavLink
+                exact
+                className="nav-link common"
+                to="/"
+                activeClassName="activeClass"
+              >
+                Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about">
+              <NavLink
+                exact
+                className="nav-link common"
+                to="/about"
+                activeClassName="activeClass"
+              >
                 About
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contect">
+              <NavLink
+                exact
+                className="nav-link common"
+                to="/contect"
+                activeClassName="activeClass"
+              >
                 Contect
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/login">
+              <NavLink
+                exact
+                className="nav-link common"
+                to="/login"
+                activeClassName="activeClass"
+                // className="common"
+              >
                 Login
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link " to="/register">
+              <NavLink
+                exact
+                className="nav-link common"
+                to="/register"
+                activeClassName="activeClass"
+              >
                 Register
               </NavLink>
             </li>
