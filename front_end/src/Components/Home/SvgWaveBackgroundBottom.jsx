151b35d2214a3179dc98cd82b88c5f9937fc66e4
Munsif <munsifalimisri69@gmail.com>
2021-04-09 23:52:05 +0500

    svg Wave footer for  homepage only

--- front_end/src/Components/Home/SvgWaveBackgroundBottom.jsx
+++ front_end/src/Components/Home/SvgWaveBackgroundBottom.jsx
@@ -3,8 +3,6 @@ import React from "react";
 function SvgWaveBackgroundBottom() {
   return (
     <svg
-      height="100%"
-      width="100%"
       id="svg"
       viewBox="0 0 1440 450"
       xmlns="http://www.w3.org/2000/svg"
