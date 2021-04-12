8038eb084fdf39a51d704cd0e1dd4dedf06a02b6
Munsif <munsifalimisri69@gmail.com>
2021-04-12 23:13:30 +0500

    added authentions part to bout only authentic  users can access the about PAGE

---
+++ server/middleware/authentication.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../schema_or_models/userSchema");
+const aboutMiddleWare = async function (req, res, next) {
+  try {
+    console.log("in middle ware");
+    console.log("cookie " + req.cookies.jsonWebToken);
+    const cookieStoredToken = req.cookies.jsonWebToken;
+    //jwt.verify(token, secretOrPublicKey, [options, callback])
+    const verifyToken = jwt.verify(cookieStoredToken, process.env.SECRET__KEY);
+    // console.log(verifyToken); jo token cookie ma save ha usko sath secret key ko match kry or pata chalai ga ka ya secret key iske cookie ke ha agr same hote ha to us cookie ka sath jitna ba data attach krwaya hoga apna like idd email etc sab mily ga
+    console.log(verifyToken.email);
+    const rootUser = await User.findOne({
+      _id: verifyToken._id,
+      "tokens.token": cookieStoredToken,
+    });
+    req.token = cookieStoredToken;
+    req.rootUser = rootUser;
+    req.id = rootUser._id;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "not Authorizes user" });
+    console.error(error);
+  }
+};
+module.exports = aboutMiddleWare;
