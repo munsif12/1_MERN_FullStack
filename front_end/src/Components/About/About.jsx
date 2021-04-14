bff79643cefcde5baab925396342f8099baa9d6b
Munsif <munsifalimisri69@gmail.com>
2021-04-14 23:09:00 +0500

    fetching user data from database with complete authentication done

--- front_end/src/Components/About/About.jsx
+++ front_end/src/Components/About/About.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./About.css";
-import { useHistory } from "react-router-dom";
+import TempUserImg from "../userImages/temporaryImage.png";
+import { useHistory, NavLink } from "react-router-dom";
 function About() {
+  const [userDbData, setSserDbData] = useState({});
   const history = useHistory();
   async function checkIfUserIsAuthenticated() {
     try {
@@ -17,7 +19,7 @@ function About() {
       if (response.status === 401) {
         history.push("/login");
       } else {
-        console.log(data);
+        data ? setSserDbData(data) : alert("not valid data");
       }
     } catch (error) {
       console.log(error);
@@ -27,33 +29,84 @@ function About() {
   useEffect(() => {
     checkIfUserIsAuthenticated();
   }, []);
+  const { _id: id, userName: name, email, phone, work, date } = userDbData;
   return (
     <div className="about">
-      <div className="aboutDetails">
-        <h1>User Details</h1>
-        <ul>
-          <li>
-            <label> Id </label> : <p> 232323232323</p>
-          </li>
-          <li>
-            <label>Name</label> : <p> Munsif</p>
-          </li>
-          <li>
-            <label>Email</label> : <p> xyz@gmail.com</p>
-          </li>
-          <li>
-            <label>Phone</label> : <p> 000001222</p>
-          </li>
-          <li>
-            <label>Profession</label> : <p> MERN STACK DEVELOPER</p>
-          </li>
-          <li>
-            <label>Name</label> : <p> Munsif</p>
-          </li>
-        </ul>
-      </div>
-      <div className="timelineDetails">
-        <h1>this is timeLine</h1>
+      <div className="about__wrapper">
+        <div className="about__upper">
+          <div className="userImage">
+            <img src={TempUserImg} alt="UserImage" srcset="" />
+          </div>
+          <div className="userName__word__dbNo">
+            <div className="userName">
+              <h1 className="userName__h1"> {name} </h1>
+            </div>
+            <div className="userWork">
+              <h4 className="userWork__h4">{work}</h4>
+            </div>
+            <div className="userDateOfRegistertion">
+              <h4 className="userDateOfRegistertion__h5">{date}</h4>
+            </div>
+          </div>
+          <div className="editProfile">
+            <p className="editProfile__p">EditProfile</p>
+          </div>
+        </div>
+        <div className="about__lower">
+          <div className="userHobbies__and__work">
+            <div className="userSocialLinks ">
+              <div className="userFacebook links__common">
+                <NavLink to="http://www.facebook.com"> Facebook</NavLink>
+              </div>
+              <div className="userTwitter links__common">
+                <NavLink to="http://www.twitter.com"> Twitter</NavLink>
+              </div>
+              <div className="userLinkedin links__common">
+                <NavLink to="http://www.linkedin.com"> LinkedIn</NavLink>
+              </div>
+              <div className="userGithub links__common">
+                <NavLink to="http://www.github.com"> Github</NavLink>
+              </div>
+              <div className="userInstagram links__common">
+                <NavLink to="http://www.instagram.com"> Instagram</NavLink>
+              </div>
+            </div>
+          </div>
+          <div className="userDb__data__titles">
+            <div className="user__id">
+              <p className="user__id__text">Id :</p>
+            </div>
+            <div className="user__name">
+              <p className="user__name__text">Name :</p>
+            </div>
+            <div className="user__email">
+              <p className="user__email__text">Email :</p>
+            </div>
+            <div className="user__phone">
+              <p className="user__phone__text">Phone :</p>
+            </div>
+            <div className="user__profession">
+              <p className="user__profession__text">Work :</p>
+            </div>
+          </div>
+          <div className="user__database__data">
+            <div className="db__id">
+              <p className="db__id__text">{id} </p>
+            </div>
+            <div className="db__name">
+              <p className="db__name__text"> {name}</p>
+            </div>
+            <div className="db__email">
+              <p className="db__email__text"> {email} </p>
+            </div>
+            <div className="db__phone">
+              <p className="db__phone__text">{phone}</p>
+            </div>
+            <div className="db__profession">
+              <p className="db__profession__text">{work}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
