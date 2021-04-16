0d27980e88dc0d9a27ee5721d42ba1cc263abd35
Munsif <munsifalimisri69@gmail.com>
2021-04-16 23:34:53 +0500

    Added both online db and offline db

--- server/db_connection/connection.js
+++ server/db_connection/connection.js
@@ -1,15 +1,27 @@
+// Online Database mongoDb atlas
 const mongoose = require("mongoose");
 //connecting to online mongodb atlas -> u hava to have mongodb installedin your local machine
 const Database = process.env.DATABASE__CONN; //to access variables inside dotenv -> config file use process.env.variableName
-mongoose.connect(
-  Database,
-  {
+mongoose
+  .connect(Database, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-  },
-  () => {
-    console.log("connection to mongodb atlas successfull");
-  }
-);
+  })
+  .then(() => {
+    console.log(`Connection successfull using MongoDb Atlas..`);
+  })
+  .catch((error) => {
+    mongoose
+      .connect("mongodb://localhost:27017/mern", {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log(`Connection successfull using uocal Db..`);
+      })
+      .catch((e) => console.log(`Connection error : ${e}`));
+  });
