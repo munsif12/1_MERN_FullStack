fd7962663a9510939512068933d1800c21c41276
Munsif <munsifalimisri69@gmail.com>
2021-04-18 22:48:10 +0500

    main App.js file

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Contect from "./Components/Contect/Contect";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
+import Footer from "./Components/Footer/Footer";
 import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Logout from "./Components/Logout/Logout";
 
 function App() {
+  const location = useLocation();
   return (
-    <div className="App">
+    <div className="App" id="bootstrap__overide_css_for__Navbar">
       <Navbar />
       <Switch>
         <Route exact path="/">
@@ -32,11 +35,16 @@ function App() {
         <Route exact path="/register">
           <Register />
         </Route>
+
+        <Route exact path="/logout">
+          <Logout />
+        </Route>
         {/* if path not exists then show the error page */}
         <Route>
           <ErrorPage />
         </Route>
       </Switch>
+      {location.pathname === "/" ? null : <Footer />}
     </div>
   );
 }
