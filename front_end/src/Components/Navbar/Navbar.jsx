4c181659bce1b90cf0c6ac105b07060a9c7d8eeb
Munsif <munsifalimisri69@gmail.com>
2021-04-21 20:59:42 +0500

    Added Registered users page/Navlink

--- front_end/src/Components/Navbar/Navbar.jsx
+++ front_end/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./Navbar.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { NavLink } from "react-router-dom";
-import anime from "animejs/lib/anime.es.js";
+import { loginLogoutContext } from "../../App";
 function Navbar() {
+  const { state } = useContext(loginLogoutContext);
   const [globalUserName, setGlobalUserName] = useState("");
   async function setUserDataToForm() {
     const response = await fetch("/contect-user", {
@@ -13,25 +14,20 @@ function Navbar() {
       },
     });
     const data = await response.json();
-    console.log(data);
     setGlobalUserName(data.userName);
   }
   useEffect(() => {
     setUserDataToForm();
-    anime({
-      targets: ".navbar_logo",
-      opacity: 1,
-      translateX: 10,
-      duration: 3000,
-      // direction: "alternate",
-    });
+    // === !== != >= <= <> += 
   });
 
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand navbar_logo" to="##">
-          <h2>{!globalUserName ? "BLM" : `${globalUserName}`}</h2>
+        <NavLink className="navbar-brand navbar_logo" to="##" id="navbar__logo">
+          <h2 style={{ margin: "0px" }}>
+            {!globalUserName ? "BLM" : `${globalUserName}`}
+          </h2>
         </NavLink>
         <button
           className="navbar-toggler"
@@ -53,6 +49,7 @@ function Navbar() {
                 className="nav-link common"
                 to="/"
                 activeClassName="activeClass"
+                id="_1"
               >
                 Home
               </NavLink>
@@ -63,6 +60,7 @@ function Navbar() {
                 className="nav-link common"
                 to="/about"
                 activeClassName="activeClass"
+                id="_2"
               >
                 About
               </NavLink>
@@ -73,41 +71,61 @@ function Navbar() {
                 className="nav-link common"
                 to="/contect"
                 activeClassName="activeClass"
+                id="_3"
               >
                 Contect
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-link common"
-                to="/login"
-                activeClassName="activeClass"
-                // className="common"
-              >
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-link common"
-                to="/register"
-                activeClassName="activeClass"
-              >
-                Register
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                className="nav-link common"
-                to="/logout"
-                activeClassName="activeClass"
-              >
-                Logout
-              </NavLink>
-            </li>
+            {!state ? (
+              <>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    className="nav-link common"
+                    to="/login"
+                    activeClassName="activeClass"
+                    id="_4"
+                  >
+                    Login
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    className="nav-link common "
+                    to="/register"
+                    activeClassName="activeClass"
+                    id="_5"
+                  >
+                    Register
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    className="nav-link common"
+                    to="/our-users"
+                    activeClassName="activeClass"
+                  >
+                    Registered Users
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    className="nav-link common"
+                    to="/logout"
+                    activeClassName="activeClass"
+                    id="logout__btn"
+                  >
+                    Logout
+                  </NavLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </nav>
