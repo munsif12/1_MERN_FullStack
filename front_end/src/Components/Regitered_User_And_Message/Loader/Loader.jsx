7b39a58591609c6cd770f6650c0a29afbe663ea5
Munsif <munsifalimisri69@gmail.com>
2021-04-24 23:13:51 +0500

    Loader before showing the Registred users

---
+++ front_end/src/Components/Regitered_User_And_Message/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Loader.css"
+function Loader() {
+    return (
+        <div className="LoaderForAllRegisteredUsers">
+            <h1>Loading</h1>
+            <span className="s1">.</span>
+            <span className="s2">.</span>
+            <span className="s3">.</span>
+        </div>
+    )
+}
+
+export default Loader
