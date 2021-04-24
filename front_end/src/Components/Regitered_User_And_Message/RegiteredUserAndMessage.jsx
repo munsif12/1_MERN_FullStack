44603cf83cc18e7aef5f7152572f7d6a9bcf4aa3
Munsif <munsifalimisri69@gmail.com>
2021-04-24 23:15:05 +0500

    Component which will show all the registered users with us in db

--- front_end/src/Components/Regitered_User_And_Message/RegiteredUserAndMessage.jsx
+++ front_end/src/Components/Regitered_User_And_Message/RegiteredUserAndMessage.jsx
@@ -1,69 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import "./RegiteredUserAndMessage.css"
+import React, { useState, useEffect } from "react";
+import "./RegiteredUserAndMessage.css";
 import { useHistory } from "react-router-dom";
+import UsersRegisteredWithUs from "./UsersData/UsersRegisteredWithUs";
+import Loader from "./Loader/Loader";
 function RegiteredUserAndMessage() {
-    const history = useHistory();
-    const [allRegisteredUsers, setAllRegisteredUsers] = useState([])
-    async function getAllUsers() {
-        try {
-            const response = await fetch("/our-users", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-            const data = await response.json();
-            if (response.status === (401 || 502 || 500) || !data) {
-                history.push("/login")
-            }
-            console.log(data);
-            setAllRegisteredUsers([...data])
-            // console.log(data);
-            console.log(typeof allRegisteredUsers);
-            console.log(allRegisteredUsers[0])
-        } catch (error) {
-            console.log(error);
-            alert("not a registered user")
-        }
+  const history = useHistory();
+  const [allRegisteredUsers, setAllRegisteredUsers] = useState([]);
+  const [showLoaing, setShowLoaing] = useState(false);
+  async function getAllUsers() {
+    try {
+      const response = await fetch("/our-users", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if ([401, 500, 500].includes(response.status) || !data) {
+        history.push("/login");
+      }
+      setShowLoaing(true);
+      setAllRegisteredUsers([...data]);
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+      alert("not a registered user");
     }
-    useEffect(() => {
-        getAllUsers();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-    return (
-        <div className="reg_user_and_feedback">
-            <div className="reg_feeback_wrapper">
-                {
-                    allRegisteredUsers.map((user, index) => {
-                        return (
-                            <div className="user">
-                                <div className="userImg">
-                                    <p>
-                                        {
-                                            // user.name.slice(0, 1)//toget first letter of Name => myOwn
-                                            //to get first letter of every word in name =>from stack overflow
-                                            user.name.split(/\s/).reduce((response, word) => response += word.slice(0, 1), '')
-                                        }
-                                    </p>
-                                </div>
-                                <div className="userDetails">
-                                    <p className="r_user_name">
-                                        {
-                                            user.name
-                                        }
-                                    </p>
-                                    <p className="r_user_email">
-                                        {
-                                            user.email
-                                        }
-                                    </p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
+  }
+  function removeUser(cardUserName) {
+    document.getElementById("showResetButton").style.display = "block";
+    setAllRegisteredUsers((preVal) => {
+      // console.log(typeof preVal)
+      // console.log(typeof allRegisteredUsers)
+      return preVal.filter((val) => {
+        return val.name !== cardUserName;
+      });
+    });
+  }
+  useEffect(() => {
+    getAllUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return showLoaing ? (
+    <UsersRegisteredWithUs
+      allRegisteredUsers={allRegisteredUsers}
+      removeUser={removeUser}
+      getAllUsers={getAllUsers}
+    />
+  ) : (
+    <Loader />
+  );
 }
-export default RegiteredUserAndMessage
+export default RegiteredUserAndMessage;
