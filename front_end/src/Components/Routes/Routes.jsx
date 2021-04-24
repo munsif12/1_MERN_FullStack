36f103ec4a34e8a60142d6ee075b0f237c82c598
Munsif <munsifalimisri69@gmail.com>
2021-04-25 01:42:21 +0500

    All the routing for FrontEnd

--- front_end/src/Components/Routes/Routes.jsx
+++ front_end/src/Components/Routes/Routes.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "../Home/Home"
+import Home from "../Home/Home";
 import About from "../About/About";
 import Contect from "../Contect/Contect";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Logout from "../Logout/Logout";
+import RegiteredUserAndMessage from "../Regitered_User_And_Message/RegiteredUserAndMessage";
 function Routes() {
-    return (
-        <>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-                <Route exact path="/about">
-                    <About />
-                </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
 
-                <Route exact path="/contect">
-                    <Contect />
-                </Route>
+        <Route exact path="/contect">
+          <Contect />
+        </Route>
 
-                <Route exact path="/login">
-                    <Login />
-                </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
 
-                <Route exact path="/register">
-                    <Register />
-                </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
 
-                <Route exact path="/logout">
-                    <Logout />
-                </Route>
-                {/* if path not exists then show the error page */}
-                <Route>
-                    <ErrorPage />
-                </Route>
-            </Switch>
-        </>
-    )
+        <Route exact path="/our-users">
+          <RegiteredUserAndMessage />
+        </Route>
+
+        <Route exact path="/logout">
+          <Logout />
+        </Route>
+        {/* if path not exists then show the error page */}
+        <Route>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
-export default Routes
+export default Routes;
