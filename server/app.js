e865ccf75958cb61a56d1e29168d1faf3dd053fa
Munsif <munsifalimisri69@gmail.com>
2021-04-26 13:00:00 +0300

    main app.js which  entry point for the backend

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-//cookie parser apka cookies ko reqest ma dalta h ise error ke waja sa 1 project miss hua or aj time b bht laga
 var cookieParser = require("cookie-parser");
-app.use(cookieParser());
+
 //dotenv ka filePath app ko batanay kaleya
 dotenv.config({ path: "./config.env" });
 //settingn up connection by requiring the connetion file the connection functionality is performed in connection.js file
@@ -12,6 +11,9 @@ require("./db_connection/connection");
 //app ko batow ka data json format ma aiy ga agr koi request postman ka zareya hui ha
 //app ko batarhy ha ka agr json ka data aiy to usko object ma convert krdo
 app.use(express.json()); //ya lines hamesha router ko require wali line sa uper hone chaheya becoz js synchroonous hota h wara mare tarah 20 min waste hongy
+
+//cookie parser apka cookies ko reqest ma dalta h ise error ke waja sa 1 project miss hua or aj time b bht laga
+app.use(cookieParser());
 
 //getting the routes for the app like app.get("/",()=>{res.send("hello server")})
 app.use(require("./routes/routes"));
